f2062e0de7bf4c0931a70cb088bca162c73c7816
breath-co2 <jonwang@queyang.com>
2016-05-31 12:00:15 +0800

    support remove_suffix and add_suffix param
    support remove_suffix and add_suffix param

--- lib/fluent/plugin/out_retag.rb
+++ lib/fluent/plugin/out_retag.rb
@@ -4,14 +4,16 @@ class Fluent::RetagOutput < Fluent::Output
   config_param :tag, :string, :default => nil
   config_param :remove_prefix, :string, :default => nil
   config_param :add_prefix, :string, :default => nil
+  config_param :remove_suffix, :string, :default => nil
+  config_param :add_suffix, :string, :default => nil
 
   def configure(conf)
     super
-    if not @tag and not @remove_prefix and not @add_prefix
-      raise Fluent::ConfigError, "missing both of remove_prefix and add_prefix"
+    if not @tag and not @remove_prefix and not @add_prefix and not @remove_suffix and not @add_suffix
+      raise Fluent::ConfigError, "missing remove_prefix and add_prefix and remove_suffix and add_suffix"
     end
-    if @tag and (@remove_prefix or @add_prefix)
-      raise Fluent::ConfigError, "both of tag and remove_prefix/add_prefix must not be specified"
+    if @tag and (@remove_prefix or @add_prefix or @remove_suffix or @add_suffix)
+      raise Fluent::ConfigError, "tag and remove_prefix/add_prefix/remove_suffix/add_suffix must not be specified"
     end
     if @remove_prefix
       @removed_prefix_string = @remove_prefix + '.'
@@ -20,12 +22,25 @@ class Fluent::RetagOutput < Fluent::Output
     if @add_prefix
       @added_prefix_string = @add_prefix + '.'
     end
+
+    if @remove_suffix
+      @removed_suffix_string = '.' + @remove_suffix
+      @removed_suffix_length = @removed_suffix_string.length
+      @removed_suffix_pos = 0 - @removed_suffix_length
+    end
+    if @add_suffix
+      @added_suffix_string = '.' + @add_suffix 
+    end
   end
 
   def emit(tag, es, chain)
     tag = if @tag
             @tag
           else
+            if @remove_suffix and
+                ((tag.end_with?(@removed_suffix_string) and tag.length > @removed_suffix_length) or tag == @remove_suffix)
+              tag = tag[0...@removed_suffix_pos]
+            end
             if @remove_prefix and
                 ( (tag.start_with?(@removed_prefix_string) and tag.length > @removed_length) or tag == @remove_prefix)
               tag = tag[@removed_length..-1]
@@ -35,6 +50,13 @@ class Fluent::RetagOutput < Fluent::Output
                       @added_prefix_string + tag
                     else
                       @add_prefix
+                    end
+            end
+            if @add_suffix 
+              tag = if tag and tag.length > 0
+                      tag + @added_suffix_string
+                    else
+                      @add_suffix
                     end
             end
             tag
